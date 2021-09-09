c505a4f8dc760b49346b90cc1bf5c7f2265c93ea
TumKtp <tumktp.dev@gmail.com>
2021-09-09 15:23:42 +0700

    Roll back

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const serverless = require("serverless-http");
-const { isSignedIn } = require("../middlewares/auth");
+const { isSignedIn } = require("./middlewares/auth");
 const app = express();
 
 // Middlewares
 app.use(express.json());
 // Routes
-const authRoutes = require("../routes/auth");
-const patientRoutes = require("../routes/patient");
+const authRoutes = require("./routes/auth");
+const patientRoutes = require("./routes/patient");
 
-app.use("/.netlify/functions/api", authRoutes);
-app.use("/.netlify/functions/api", isSignedIn, patientRoutes);
+app.use("/api", authRoutes);
+app.use("/api", isSignedIn, patientRoutes);
 
 // const port = process.env.PORT || 5000;
 // app.listen(port, () => {
