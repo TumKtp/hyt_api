0e453616e51bff4d16d8881e14bda02b90f8211c
TumKtp <tumktp.dev@gmail.com>
2021-09-09 16:15:45 +0700

    Init

--- db/index.js
+++ db/index.js
@@ -1,6 +1,8 @@
 const { Pool } = require("pg");
-const connectionString = process.env.PGCONNECTIONSTRING;
-const pool = new Pool({ connectionString });
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({
+  connectionString,
+});
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
