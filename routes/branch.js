eeada1317015d287a6347bc42456cd8876708d19
TumKtp <tumktp.dev@gmail.com>
2021-10-06 19:42:08 +0700

    Change branch config

--- routes/branch.js
+++ routes/branch.js
@@ -8,14 +8,15 @@ const {
   deleteBranch,
   getBranch,
 } = require("../controllers/branch");
+const { isSignedIn } = require("../middlewares/auth");
 
-router.post("/branches", addBranch);
+router.post("/branches", isSignedIn, addBranch);
 
 router.get("/branches/:branchId", getBranch);
 router.get("/branches", getAllBranches);
 
-router.put("/branches/:branchId", updateBranch);
+router.put("/branches/:branchId", isSignedIn, updateBranch);
 
-router.delete("/branches/:branchId", deleteBranch);
+router.delete("/branches/:branchId", isSignedIn, deleteBranch);
 
 module.exports = router;
